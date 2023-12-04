33215cdad71f628d7985c6a478bb6abafeeb9d68
Sravan T P <sravantp99@gmail.com>
2023-12-04 21:02:49 +0530

    improved toggle button

--- client/app/_components/ThemeToggle/ThemeToggle.jsx
+++ client/app/_components/ThemeToggle/ThemeToggle.jsx
@@ -9,9 +9,24 @@ export default function ThemeToggle() {
   const { theme, toggleTheme } = useContext(ThemeContext);
 
   return (
-    <div className={styles.container} onClick={toggleTheme}>
+    <div
+      className={styles.container}
+      onClick={toggleTheme}
+      style={
+        theme === "dark"
+          ? { background: "white" }
+          : { right: "1px", background: "black" }
+      }
+    >
       <Image src={"/sun.png"} alt="sun" width={14} height={14} />
-      <div className={styles.circle}></div>
+      <div
+        className={styles.circle}
+        style={
+          theme === "dark"
+            ? { left: "1px", background: "#0f172a" }
+            : { right: "1px", background: "white" }
+        }
+      ></div>
       <Image src={"/moon.png"} alt="moon" width={14} height={14} />
     </div>
   );
