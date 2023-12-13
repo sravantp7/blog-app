3b444520d69e229cd269545201c1771b2842e0e3
Sravan T P <sravantp99@gmail.com>
2023-12-13 20:28:06 +0530

    structure feature component

--- client/app/_components/Featured/Featured.jsx
+++ client/app/_components/Featured/Featured.jsx
@@ -1,3 +1,29 @@
+import styles from "./Featured.module.css";
+import Image from "next/image";
+
 export default function Featured() {
-  return <div>Featured</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, Dev here!</b> Discover my stories and creative ideas.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="" fill />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus,
+            veniam.
+          </h1>
+          <p className={styles.postDescription}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam unde
+            similique vitae blanditiis asperiores deserunt perspiciatis eaque
+            praesentium amet recusandae.
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
 }
